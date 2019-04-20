a8d30f493ae1766cb09adf9e00901f719fd4a65e
Aphelion0 <cs17b101@smail.iitm.ac.in>
2019-04-20 15:27:12 +0530

    Binded Age,Interest & Distance to Component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+    
+    public interest:string = "";
+    public agelow: number = 21;
+    public agehigh:number = 21;
+    public maxdist:string = "0km";
+  
+    constructor(){
+
+    }
+
+    ngOnInit(){
+      this.interest = "Both";
+      //console.log(this.interest);
+    }
+
+    UpdateDistance(event:any){
+      this.maxdist = event.detail.value+"km";
+      if(event.detail.value>="100")
+      this.maxdist+="+";
+    }
+
+    UpdateAge(event:any){
+      //console.log(event.detail.value);
+      this.agelow = event.detail.value.lower;
+      this.agehigh = event.detail.value.upper;
+    }
+
+    UpdateInterest(event:any){
+      //console.log(event.detail.value);
+      this.interest = event.detail.value;
+    }
 
 }
